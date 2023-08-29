2d892d0f20bc80a8590c36c755d4297da8a9e4a2
Mimi Starnes <mstarnes@charter.net>
2023-08-28 20:18:12 -0400

    Sprint 12 first submission for reviewer

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const app = express();
 mongoose.connect(
   "mongodb://127.0.0.1:27017/wtwr_db",
   (r) => {
-    console.log("connected to DB", r);
+    console.log("connected to db", r);
   },
-  (e) => console.log("DB error", e),
+  (e) => console.log("db error", e),
 );
 
 const routes = require("./routes");
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   req.user = {
-    _id: "64ebfb4a3ffd45e4821ed88f", // paste the _id of the test user created in the previous step
+    _id: "64ed326ef9c179238e0e7459", // paste the _id of the test user created in the previous step
   };
   next();
 });
